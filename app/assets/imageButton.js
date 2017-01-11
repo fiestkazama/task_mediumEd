8224dacc9953c78de79bbff4454abb5b6d27e0e6
Devojyoti Nandi <fiestkazama@users.noreply.github.com>
2017-01-11 10:15:53 +0530

    Update imageButton.js

--- app/assets/imageButton.js
+++ app/assets/imageButton.js
@@ -14,6 +14,7 @@ import 'isomorphic-fetch';
 
 export default class ImageButton extends ImageSideButton  {
 
+  //If doesn't work.. place it outside class..
   static propTypes: {
     setEditorState: PropTypes.func,
     getEditorState: PropTypes.func,
